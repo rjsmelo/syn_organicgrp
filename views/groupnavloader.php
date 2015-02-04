10e6688ce5bda8d0441a733dd985a9471415d133
Nelson Ko <nelson@citadelrock.com>
2015-02-04 00:08:35 -0500

    [FIX] Addon Views now follow Tiki wiki-plugin style info for params

--- views/groupnavloader.php
+++ views/groupnavloader.php
@@ -2,6 +2,25 @@
 
 namespace tikiaddon\syn\organicgrp;
 
+function groupnavloader_info()
+{
+	return array(
+		'name' => tra('Group Navigation Loader'),
+		'description' => tra('Display the Organic Group Navbar'),
+		'filter' => 'text',
+		'params' => array(
+			'from' => array(
+				'required' => false,
+				'name' => tra('Where it is placed'),
+				'description' => tra('This is used to set the current page to highlight the current button'),
+				'filter' => 'text',
+			),
+		),
+	);
+
+}
+
+
 function groupnavloader($data, $params)
 {
 	if (!isset($params['from'])) {
