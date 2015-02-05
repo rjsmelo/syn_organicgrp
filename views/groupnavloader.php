6ade956b3f01da9ea6a52fe157fc54258c858316
SysAdm <patrick@synergiqsolutions.com>
2015-02-05 17:27:24 -0500

    Revert "Make use of core getNavBar"
    This reverts commit c13cc3781471ea041a995e2824f5607d6c6fd28e.

--- views/groupnavloader.php
+++ views/groupnavloader.php
@@ -27,8 +27,49 @@ function groupnavloader($data, $params)
 		return 'Please specify parameter: from';
 	}
 
-	$navapi = new \TikiAddons_Api_NavBar;
-	$navbar = $navapi->getNavBar('syn_organicgrp', $params['from']);
+	$app = \TikiAddons::get('syn_organicgrp');
+	$api = new \TikiAddons_Api_Group;
 
-	return $navbar;
+	if (!isset($_REQUEST['organicgroup']) && !empty($_REQUEST['page'])) {
+		$info = $api->getOrganicGroupInfoForItem('wiki page', $_REQUEST['page']);
+		$cat = $info['cat'];
+		$ogid = $info['organicgroup'];
+		$app->smarty->assign('groupTrackerItemId', $ogid);
+		$_REQUEST['organicgroup'] = $ogid;
+		if (!isset($_REQUEST['cat'])) {
+			$_REQUEST['cat'] = $cat;
+		}
+	} 
+
+	if (!isset($_REQUEST['organicgroup']) && !empty($_REQUEST['itemId'])) {
+		$info = $api->getOrganicGroupInfoForItem('trackeritem', $_REQUEST['itemId']);
+		$cat = $info['cat'];
+		$ogid = $info['organicgroup'];
+		$app->smarty->assign('groupTrackerItemId', $ogid);
+		$_REQUEST['organicgroup'] = $ogid;
+		if (!isset($_REQUEST['cat'])) {
+			$_REQUEST['cat'] = $cat;
+		}
+	}
+
+	if (!empty($_REQUEST['organicgroup']) && empty($_REQUEST['cat'])) {
+		$ogname = 'syn_organicgrp_' . $_REQUEST['organicgroup'];
+		$cat = \TikiLib::lib('categ')->get_category_id($ogname);
+		$_REQUEST['cat'] = $cat;
+	}
+
+	// if og is private, always redirect to home page
+	if (isset($_REQUEST['organicgroup'])) {
+		$ogname = 'syn_organicgrp_' . $_REQUEST['organicgroup'];
+		global $user;
+		if ($_REQUEST['page'] != 'syn_organicgrp_grouphomepage' && $api->organicGroupIsPrivate($ogname) && !\TikiLib::lib('user')->user_is_in_group($user, $ogname)) {
+			header( 'Location: tiki-index.php?page=syn_organicgrp_grouphomepage&itemId=' . $_REQUEST['organicgroup']);
+		}
+	}
+
+	$userlib = \TikiLib::lib('user');
+	$approvalCount = $userlib->nb_users_in_group("syn_organicgrp_pending_". $_REQUEST['organicgroup']); //get the number of pending users
+	$app->smarty->assign('approvalcount', $approvalCount);
+	$app->smarty->assign('groupnavfrom', $params['from']);
+	return $app->smarty->fetch('syn-groupnavloader.tpl');
 }
